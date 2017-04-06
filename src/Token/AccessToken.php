eb5cdf23be61149da97af519449a215388645af7
Alexandros Keramidas <dev.alexkeramidas@gmail.com>
2017-04-06 11:51:08 +0300

    Added comment about JWT library validations

--- src/Token/AccessToken.php
+++ src/Token/AccessToken.php
@@ -25,7 +25,7 @@ class AccessToken extends \League\OAuth2\Client\Token\AccessToken
 
     public function __construct(array $options = [], $provider, $clientSecret)
     {
-            if (!isset($clientSecret)) {
+        if (!isset($clientSecret)) {
             throw new InvalidArgumentException('Please use the parent constructor with only one argument as a client_secret is needed for this one');
         }
 
@@ -34,40 +34,42 @@ class AccessToken extends \League\OAuth2\Client\Token\AccessToken
 
         JWT::$leeway = 60;
 
-        if(!empty($options['id_token'])){
+        if (!empty($options['id_token'])) {
             $this->idToken = $options['id_token'];
             $this->idTokenClaims = null;
             try {
                 $tokens = explode('.', $this->idToken);
-                // Check if the id_token contains signature
-                if(count($tokens) == 3 && !empty($tokens[2])) {
+                // Check if the id_token contains signature and try to decode it.
+                if (count($tokens) == 3 && !empty($tokens[2])) {
                     $idTokenClaims = (array)JWT::decode($this->idToken, $clientSecret, $provider->getProviderAlgorithm());
                 }
-            }  catch (Exception $e) {
+            } catch (Exception $e) {
                 throw new RuntimeException("Unable to decode the id_token! The secret or the encryption algorithm used is incorrect");
             }
 
         }
 
-        if ($provider->getClientId() != $idTokenClaims['aud']){
+        /**
+         * Authentiq validations for the jwt (audience, sub and issuer)
+         * The JWT library used also checks for
+         * Empty key
+         * Not allowed, unsupported or empty algorithm
+         * Incorrect number of segments
+         * Incorrect header encoding
+         * Signature verification
+         * If the nbf, iat, exp in conjunction with the leeway are defined and valid.
+         */
+
+        if ($provider->getClientId() != $idTokenClaims['aud']) {
             throw new RuntimeException('Invalid audience');
         }
 
-        if($idTokenClaims['nbf'] > time() || $idTokenClaims['exp'] < time()) {
-            // Additional validation is being performed in firebase/JWT itself
-            throw new BeforeValidException("The id token is invalid!");
-        }
-
-        if($idTokenClaims['sub'] == null){
+        if ($idTokenClaims['sub'] == null) {
             throw new RuntimeException("The id token's sub is invalid!");
         }
 
-        if($idTokenClaims['iss'] == null || $idTokenClaims['iss']  != $provider->getDomain()){
+        if ($idTokenClaims['iss'] == null || $idTokenClaims['iss'] != $provider->getDomain()) {
             throw new RuntimeException("The id token's issuer is invalid!");
-        }
-
-        if($idTokenClaims['iat'] == null){
-            throw new RuntimeException("The id token's issued time is null!");
         }
 
         $this->idTokenClaims = $idTokenClaims;
